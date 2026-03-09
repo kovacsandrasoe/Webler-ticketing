f8cce9b43083f82c0870ec15463c952b749c2422
kovacsandrasoe <kovacs.andras@uni-obuda.hu>
2026-03-09 17:29:35 +0100

    task01: ticket class created

---
+++ script.js
@@ -0,0 +1,9 @@
+//1) ticket class létrehozása (id - string(uuid), description - string, user - string, createdAt - Date, response - string, isClosed - bool)
+class Ticket{
+    id;
+    description;
+    user;
+    createdAt;
+    response;
+    isClosed;   
+}
