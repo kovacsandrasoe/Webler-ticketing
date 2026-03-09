e222a518c62d7ec3f16feb046e75ba37029c0a0c
kovacsandrasoe <kovacs.andras@uni-obuda.hu>
2026-03-09 18:10:46 +0100

    task08: display() done

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ class Ticket{
         this.id = crypto.randomUUID();
         this.createdAt = new Date();
         this.isClosed = false;
+        this.response = '';
     }
 }
 
@@ -21,6 +22,8 @@ tickets.push(new Ticket('Nem megy a lift', 'Kovács András'));
 tickets.push(new Ticket('Gazos a kert', 'Nagy Eszter'));
 tickets.push(new Ticket('Nem ég a lámpa', 'Kiss Géza'));
 tickets.push(new Ticket('Nem működik a garázskapu', 'Juhász Kálmánné'));
+tickets[2].isClosed = true;
+display();
 
 //04) save függvény létrehozása
 function save(){
@@ -30,4 +33,21 @@ function save(){
 //05) load függvény létrehozása
 function load(){
     tickets = JSON.parse(localStorage.getItem('tickets') ?? '[]');
+}
+
+//08) display függvény a táblázat rajzolására
+function display(){
+    document.getElementById('tbody').innerHTML = '';
+    tickets.forEach(x => {
+        document.getElementById('tbody').innerHTML += 
+        `
+            <tr class="table-${x.isClosed ? 'success' : 'danger'}">
+                <td>${x.description}</td>
+                <td>${x.user}</td>
+                <td>${x.createdAt.toLocaleDateString('hu-HU')} ${x.createdAt.toLocaleTimeString('hu-HU')}</td>
+                <td>${x.response}</td>
+                <td><button class="btn btn-sm btn-warning ${x.isClosed ? 'disabled' : ''}">Lezárás</button></td>
+            </tr>
+        `;
+    });
 }
